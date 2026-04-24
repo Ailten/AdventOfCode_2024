e06b1d7ea1e3091fa61cfdc2e1286015f38fdca8
Ailten <faouzi26b@live.fr>
2026-04-24 12:21:54 +0200

    exo 3 finish

--- 03/main.py
+++ 03/main.py
@@ -1,23 +1,21 @@
 import re
 
 # exo 3 :
-# 
+# take a file text, into there are a patern 'mul(1,1)' with two number, take all the num and multiply, return the sum of all.
 
 # read data.
-def readInput(path='02\\input.txt'):
+def readInput(path='03\\input.txt'):
     with open(path, 'r') as f:
         for l in f:
             yield l.strip()
 
 l = list(readInput())
-l = [ re.findall('mul([0-9]{1,},[0-9]{1,})', line) for line in l ]
+arr2_of_mul = [ re.findall('mul[(][0-9]{1,}[,][0-9]{1,}[)]', line) for line in l ]
 
 total = 0
-for line in l:
-    for m in line:
-        values = re.findall('[0-9]{1,}', m)
+for arr1_mul in arr2_of_mul:
+    for mul in arr1_mul:
+        values = re.findall('[0-9]{1,}', mul)
         total += int(values[0]) * int(values[1])
 
 print(total)
-
-# fix : return zero.
