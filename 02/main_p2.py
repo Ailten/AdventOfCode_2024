83bdb59b78fc24c8a9b3231a44d6aecb69c0756d
Ailten <faouzi26b@live.fr>
2026-04-25 15:07:11 +0200

    exo 02 p2 resolve

--- 02/main_p2.py
+++ 02/main_p2.py
@@ -10,49 +10,16 @@ def readInput(path='02\\input.txt'):
         for l in f:
             yield l.strip()
 
-#def isLineValid(line: list, is_has_jocker: bool=False) -> int:
-#    
-#    values = re.findall('[0-9]{1,}', line)
-#    values = [ int(e) for e in values ]
-#
-#    if is_has_jocker:
-#        if isLineValid(' '.join([ str(e) for e in values[:1] + values[2:] ])) == 0:
-#            return 0
-#        if isLineValid(' '.join([ str(e) for e in values[1:] ])) == 0:
-#            return 0
-#        if isLineValid(' '.join([ str(e) for e in values[:-1] ])) == 0:
-#            return 0
-#    
-#    last_element = values[0]
-#    is_increasing = values[0] < values[1]
-#    for i in range(1, len(values)):
-#        e = values[i]
-#        dif = e - last_element
-#        current_is_increase = last_element < e
-#
-#        # check all increase (or not).
-#        if is_increasing != current_is_increase:
-#            if is_has_jocker:
-#                is_has_jocker = False
-#                continue
-#            return 1
-#
-#        dif = abs(dif)
-#        if dif < 1 or dif > 3:
-#            if is_has_jocker:
-#                is_has_jocker = False
-#                continue
-#            return 2
-#        
-#        last_element = e
-#
-#    return 0
 
-
-
-def isLineValid(line: str, is_jocker: bool=False):
+def isLineValid(line: str, is_jocker: bool=False) -> bool:
     values = re.findall('[0-9]{1,}', line)
     values = [ int(e) for e in values ]
+
+    if is_jocker:
+        if isLineValid(' '.join([ str(e) for e in values[1:] ])):
+            return True
+        if isLineValid(' '.join([ str(e) for e in values[:-1] ])):
+            return True
 
     is_increase = values[0] < values[1]
 
@@ -62,11 +29,7 @@ def isLineValid(line: str, is_jocker: bool=False):
             if is_jocker:
                 if isLineValid(' '.join([ str(e) for e in values[:i]+values[i+1:] ])):
                     return True
-                if isLineValid(' '.join([ str(e) for e in values[:i+1]+values[i+2:] ])):
-                    return True
-                if i <= 2 and isLineValid(' '.join([ str(e) for e in values[1:] ])):
-                    return True
-                if i <= 2 and isLineValid(' '.join([ str(e) for e in values[:1]+values[2:] ])):
+                if isLineValid(' '.join([ str(e) for e in values[:i-1]+values[i:] ])):
                     return True
             return False
 
@@ -75,11 +38,7 @@ def isLineValid(line: str, is_jocker: bool=False):
             if is_jocker:
                 if isLineValid(' '.join([ str(e) for e in values[:i]+values[i+1:] ])):
                     return True
-                if isLineValid(' '.join([ str(e) for e in values[:i+1]+values[i+2:] ])):
-                    return True
-                if i <= 2 and isLineValid(' '.join([ str(e) for e in values[1:] ])):
-                    return True
-                if i <= 2 and isLineValid(' '.join([ str(e) for e in values[:1]+values[2:] ])):
+                if isLineValid(' '.join([ str(e) for e in values[:i-1]+values[i:] ])):
                     return True
             return False
         
@@ -89,28 +48,6 @@ def isLineValid(line: str, is_jocker: bool=False):
 
 
 l = list(readInput())
-l = [
-    '1 1 2 3 4 5',
-    '1 2 3 4 5 5',
-    '5 1 2 3 4 5',
-    '1 4 3 2 1',
-    '1 6 7 8 9',
-    '1 2 3 4 3',
-    '9 8 7 6 7',
-    '7 10 8 10 11'
-]
 l_valide = [ f'{line}' for line in l if isLineValid(line, True) ]
 print(len(l_valide))
 
-# 332
-# 373 to low
-# 998 to height
-# 764 to height  # FIXME, still not valide.
-# 375 x (?)
-# 381 x (?)
-# 397 x
-# 388 x
-# 393
-# 397 x
-
-# (expect 398)
